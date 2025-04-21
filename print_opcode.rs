b9bdbea32bc4992a6a730b648d961480b33664c4
EGL20FL <147153225+EGL20FL@users.noreply.github.com>
2025-04-21 17:09:02 +0500

    Update print_opcode.rs

--- print_opcode.rs
+++ print_opcode.rs
@@ -1,4 +1,5 @@
 fn main(){
+let opcode = 0xB123;
         let c = ((opcode & 0xF000) >> 12) as u8;
         let x = ((opcode & 0x0F00) >>  8) as u8;
         let y = ((opcode & 0x00F0) >>  4) as u8;
