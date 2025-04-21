a1fbc336d54a89b67c41a0d7ef21c41b506a367d
EGL20FL <147153225+EGL20FL@users.noreply.github.com>
2025-04-21 16:49:35 +0500

    Update shell.rs

--- shell.rs
+++ shell.rs
@@ -1,4 +1,8 @@
 //echo '488d35140000006a01586a0c5a4889c70f056a3c5831ff0f05ebfe68656c6c6f20776f726c640a' | xxd -r -p >shell.bin
+//objdump -D -b binary -mi386 -Mx86-64 -Mintel shell.bin
+//u will see some magic of section data
+
+// below is wrapper to execute shell.bin in os space (linux in this case)
 use std::mem;
 
 // we do this trick because otherwise only the reference is in the .text section
