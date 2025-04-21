6efe5901adf4c26f4af2e52087ee4763c52afa7b
EGL20FL <147153225+EGL20FL@users.noreply.github.com>
2025-04-21 16:10:48 +0500

    Create print_opcode.rs

---
+++ print_opcode.rs
@@ -0,0 +1,12 @@
+fn main(){
+        let c = ((opcode & 0xF000) >> 12) as u8;
+        let x = ((opcode & 0x0F00) >>  8) as u8;
+        let y = ((opcode & 0x00F0) >>  4) as u8;
+        let d = ((opcode & 0x000F) >>  0) as u8;
+let nnn = opcode & 0x0FFF;
+println!("{:#08x}",c);
+println!("{:#08x}",x);
+println!("{:#08x}",y);
+println!("{:#08x}",d);
+println!("{:#08x}",nnn);
+}
